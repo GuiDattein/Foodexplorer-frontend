5bb92a87824b55fe12e4912a6784c1873442780d
Guilherme Dattein <guidattein12@gmail.com>
2023-08-08 13:45:32 -0300

    testando commit em outro ambiente

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
-import { Prato } from './pages/Prato';
+import { Home } from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Prato />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
